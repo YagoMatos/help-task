9f51f666c4a1684ccca04be89ca018f0fc9b392e
yagomatos <yago.matos@hotmail.com>
2018-01-18 15:39:04 -0200

    frontend with react without redux and integration with api of backend

---
+++ frontend/src/helpTask/helpTask.jsx
@@ -0,0 +1,91 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+import PageHeader from '../template/pageHeader'
+import TaskForm from './taskForm'
+import TaskList from './taskList'
+
+
+const URL = 'http://localhost:3003/api/tasks'
+
+
+export default class HelpTask extends Component {
+    constructor(props){
+        super(props)
+        this.state = { description: '', list: [] }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleSearch = this.handleSearch.bind(this)
+        this.handleClear = this.handleClear.bind(this)
+
+        this.handleRemove = this.handleRemove.bind(this)
+        this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
+        this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
+        this.refresh()
+    }
+    //description_regex - same thing that like(=) in SQL
+    refresh(description = '') {
+        const search = description ? `&description__regex=/${description}/` : ''
+        axios.get(`${URL}?sort=-creationData${(search)}`)
+            .then(resp => this.setState({
+                ...this.state,
+                description,
+                list: resp.data
+            }))
+    }
+
+    handleSearch(){
+        this.refresh(this.state.description)
+    }
+    
+    handleChange(e){ this.setState({
+            ...this.state, 
+            description: e.target.value
+        })
+    }
+
+    handleAdd(){
+        const description = this.state.description
+        axios.post(URL, {description})
+            .then(resp => this.refresh())
+    }   
+
+    handleRemove(task){
+        axios.delete(`${URL}/${task._id}`)
+            .then(resp => this.refresh(this.state.description))
+    }
+
+    handleMarkAsDone(task){
+        axios.put(`${URL}/${task._id}`, {...task, done: true})
+            .then(resp => this.refresh(this.state.description))
+    }
+
+    handleMarkAsPending(task){
+        axios.put(`${URL}/${task._id}`, {...task, done:false})
+            .then(resp => this.refresh(this.state.description))
+    }
+
+    handleClear(){
+        this.refresh()
+    }
+
+    render(){
+        return (
+            <div>
+                <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
+                <TaskForm
+                    description={this.state.description}
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd}
+                    handleSearch={this.handleSearch} 
+                    handleClear={this.handleClear}/>
+                <TaskList 
+                    list={this.state.list}
+                    handleMarkAsDone={this.handleMarkAsDone}
+                    handleMarkAsPending={this.handleMarkAsPending}
+                    handleRemove={this.handleRemove}/>
+            </div>
+        )
+    }
+}
---
+++ frontend/src/main/routes.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+//hashHistory to navigation 
+import { Router, Route, Redirect, hashHistory } from 'react-router'
+
+import HelpTask from '../helpTask/helpTask'
+import About from '../about/about'
+
+export default props => (
+    <Router history={hashHistory}>
+        <Router path='/helptasks' component={HelpTask}/>
+        <Router path='/about' component={About}/>
+
+        {/*Every time that there is invalid url, then redirect to home*/}
+        <Redirect from='*' to='/helptasks'/>
+    </Router>
+)
