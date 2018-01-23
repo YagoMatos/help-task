55511f3cd1d8247629cce26b3da0902e363a63e6
yagomatos <yago.matos@hotmail.com>
2018-01-23 02:08:52 -0200

    incluing redux part2 - join react + redux completed

--- frontend/src/task/taskActions.js
+++ frontend/src/task/taskActions.js
@@ -8,9 +8,43 @@ export const changeDescription = event => ({
 })
 
 export const search = () => {
-    const request = axios.get(`${URL}?sort=-creationData`)
-    return {
-        type: 'TASK_SEARCHED',
-        payload: request
+    return (dispatch, getState) => {
+        const description = getState().task.description
+        const search = description ? `&description__regex=/${description}/` : ''
+        const request = axios.get(`${URL}?sort=-creationData${search}`)
+            .then(resp => dispatch({type: 'TASK_SEARCHED', payload: resp.data}))
     }
+}
+
+export const add = ( description ) => {
+    return dispatch => {
+        axios.post(URL, { description })
+            .then(resp => dispatch(clear()))
+            .then(resp => dispatch(search()))
+    }
+}
+
+export const markAsDone = (task) => {
+    return dispatch => {
+        axios.put(`${URL}/${task._id}`, { ...task, done: true })
+            .then(resp => dispatch(search()))
+    }
+}
+
+export const markAsPending = (task) => {
+    return dispatch => {
+        axios.put(`${URL}/${task._id}`, {...task, done: false})
+            .then(resp => dispatch(search()))
+    }
+}
+
+export const remove = (task) => {
+    return dispatch => {
+        axios.delete(`${URL}/${task._id}`)
+            .then(resp => dispatch(search()))
+    }
+}
+
+export const clear = () => {
+    return [{ type: 'TASK_CLEAR' }, search()]
 }
